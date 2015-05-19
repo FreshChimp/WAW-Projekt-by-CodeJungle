0dbef956392ccfa886aa082cc82666b20ab61401
DOGA23 <dom.gantz@gmail.com>
2015-05-19 22:01:53 +0200

    form validation

--- js/validate.js
+++ js/validate.js
@@ -1,81 +1,76 @@
-/**
- * Created by DO on 19.05.2015.
- */
 
-function pruefen(bookForm){
-
-    var form = document.getElementById(bookForm);
-    var fehlertext = "Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben.";
-    var korrekt = false;
-    var fehlerspeicher = [];
-
-    //fehlerspeicher leeren
-    fehlerspeicher.length = 0;
-
-    //felder auf richtige angaben pruefen
-    var titel = form.elements['titel'];
-    if (!istBuchstabe(titel.value)) {
-        fehlerspeicher.push(titel);
-    }
-
-    var autor = form.elements['autor'];
-    if (!istBuchstabe(autor.value)) {
-        fehlerspeicher.push(autor);
-    }
-
-    var isbn = form.elements['isbn'];
-    if (!istZahl(isbn.value)) {
-        fehlerspeicher.push(isbn);
-    }
+function validate() {
+    var form = document.forms[0];
+    var error = false;
 
 
-    var kapitel = form.elements['kapitel'];
-    if (!istZahl(kapitel.value)) {
-        fehlerspeicher.push(kapitel);
-    }
-
-    var jahr = form.elements['jahr'];
-    if (!istZahl(jahr.value)) {
-        fehlerspeicher.push(jahr);
-    }
-
-    var auflage = form.elements['auflage'];
-    if (!istZahl(auflage.value)) {
-        fehlerspeicher.push(auflage);
-    }
-
-    //focus auf den ersten fehler setzen und fehlertext ausgeben
-    if (fehlerspeicher.length != 0) {
-        //alle fehler rot umranden
-        for (i in fehlerspeicher) {
-            fehlerspeicher[i].classList.add('fehler');
+    with(document) {
+        // Autor Kontrolle
+        var regText = /^[\w ]+$/;
+        if(!regText.test(getElementById("autor").value)) {
+            getElementById("titel").style.borderColor = "red";
+            getElementById("titel").value = "";
+            getElementById("titel").focus();
+            alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie Ihre Eingaben");
+            error = true;
         }
-        fehlerspeicher[0].focus();
-        alert(fehlertext);
-    }else{
-        korrekt = true;
+
+        // ISBN Kontrolle
+        var regIsbn = /^[0-9]+$/;
+        if(!regIsbn.test(getElementById("isbn").value.length > 13 || !reg.test(getElementById("isbn").value))) {
+            getElementById("isbn").style.borderColor = "red";
+            getElementById("isbn").value = "";
+            getElementById("isbn").focus();
+            alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie Ihre Eingaben");
+            error = true;
+        }
+        // Kapitel Kontrolle
+        var regKapitel = /^[0-9]+$/;
+        if(!regKapitel.test(getElementById("kapitel").value || getElementById("kapitel").value.lentgh != 1)) {
+            getElementById("kapitel").style.borderColor = "red";
+            getElementById("kapitel").value = "";
+            getElementById("kapitel").focus();
+            alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie Ihre Eingaben");
+            error = true;
+        }
+        // Jahr Kontrolle
+        var regJahr = /^[0-9]+$/;
+        if(!reg.test(getElementById("jahr").value) || getElementById("jahr").value.length != 4) {
+            getElementById("jahr").style.borderColor = "red";
+            getElementById("jahr").value = "";
+            getElementById("jahr").focus();
+            alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie Ihre Eingaben");
+            error = true;
+        }
+        // Auflage Kontrolle
+        var regAuflage = /^[0-9]+$/);
+        if(!reg.test(getElementById("auflage").value || getElementById("auflage").value.length != 1)) {
+            getElementById("auflage").style.borderColor = "red";
+            getElementById("auflage").value = "";
+            getElementById("auflage").focus();
+            alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie Ihre Eingaben");
+            error = true;
+        }
+        // Vorname Kontrolle
+        if(!regText.test(getElementById("vorname").value)) {
+            getElementById("vorname").style.borderColor = "red";
+            getElementById("vorname").value = "";
+            getElementById("vorname").focus();
+            alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie Ihre Eingaben");
+            error = true;
+        }
+        // Nachname Kontrolle
+        if(!regText.test(getElementById("name").value)) {
+            getElementById("name").style.borderColor = "red";
+            getElementById("name").value = "";
+            getElementById("name").focus();
+            alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie Ihre Eingaben");
+            error = true;
+        }
     }
-
-    return korrekt;
-}
-
-/**
- * Diese Funktion gibt true zurueck wenn die uebergebene zeichenkette nur aus zahlen besteht
- * und false wenn nicht.
- * @param zahlen
- * @returns {boolean}
- */
-function istZahl(zahlen) {
-    var ergebnis = zahlen.match("[0-9]{1,}");
-    return ergebnis == zahlen;
-}
-
-/**
- * Diese Funkion prueft ob die uebergebene zeichenkette NUR aus buchstaben besteht
- * @param buchstaben die uebergebene zeichenkette
- * @returns {boolean}
- */
-function istBuchstabe(buchstaben){
-    var ergebnis = buchstaben.match("[A-Za-z]{1,}");
-    return ergebnis == buchstaben;
-}
+    if(!error) {
+        error = false;
+        form.action = "http://martinakraus.net/createBookEntry.php";
+        form.submit();
+    }
+};
